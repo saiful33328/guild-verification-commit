bd327471a644bafe47f2ea361b8de813dd63f2f8
saiful33328 <136515349+saiful33328@users.noreply.github.com>
2026-05-17 11:08:33 +0600

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-05-17T05:08:20.609Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-05-17T05:08:32.297Z
+Commit: Fix CSS styling bug on mobile devices
